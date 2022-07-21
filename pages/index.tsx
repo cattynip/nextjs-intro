986d9347ff63e2b48934e51176586fa0762aea0b
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-07-21 22:31:35 +0900

    1.2 Static Pre Rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 function Home() {
+  const [counter, setCounter] = useState(1);
+
+  const increaseCounter = () => {
+    setCounter((prev) => prev + 1);
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World {counter}</h1>
+      <button onClick={increaseCounter}>+</button>
     </div>
   );
 }
